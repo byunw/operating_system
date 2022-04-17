122d98acefbf0c07fb65ae4accf811da4dbde660
byunw <wbyun1461@gmail.com>
2022-04-18 08:01:20 +0900

    Update wait_for_childprocess.c

--- wait_for_childprocess.c
+++ wait_for_childprocess.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <time.h>
 
+
+//child process는 parent process랑 다른 메모리공간을 차지함 
+
+
 //parent 프로세스가 먼저 실행될지 child 프로세스가 먼저 실행될지 모름 
 //하지만 parent 프로세스가 먼저 실행될경우, wait(NULL)을 사용해서 parent 프레세스를 블록하고
 //child 프로세스가 실행된다. 그 결과 다음과 같이 터미널에 출력된다.
