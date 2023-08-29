13b9518808762750a339205bc207f60d58c65056
byunw <wbyun1461@gmail.com>
2023-08-29 22:14:33 +0900

    쓰레드 1 코드 구현

---
+++ thread/multithreading/MultithreadThing.java
@@ -0,0 +1,15 @@
+public class MultithreadThing extends Thread{
+      @Override 
+      public void run(){
+             for(int i=5;i>0;i--){
+                 System.out.println("Thread #1 : "+i);
+                 try{
+                     Thread.sleep(1000);
+                 }
+                 catch(InterruptedException e){
+                       e.printStackTrace();
+                 }
+
+             }
+      }
+}
