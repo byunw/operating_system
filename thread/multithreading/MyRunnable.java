426c3728a91120bc817f65b8d682e44ffd6430c6
byunw <wbyun1461@gmail.com>
2023-08-29 22:17:35 +0900

    Thread 2 코드 구현

---
+++ thread/multithreading/MyRunnable.java
@@ -0,0 +1,14 @@
+public class MyRunnable implements Runnable{
+       @Override 
+       public void run(){
+              for(int i=0;i<5;i++){
+                  System.out.println("Thread #2: "+i);
+                  try{
+                      Thread.sleep(1000);
+                  }
+                  catch(InterruptedException e){
+                        e.printStackTrace();
+                  }
+              }
+       }
+}
