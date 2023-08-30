7573fdbb3228a54d15bdfd6b198dddc4264bd708
byunw <wbyun1461@gmail.com>
2023-08-30 14:36:14 +0900

    인덴트 수정

--- thread/multithreading/MultithreadThing.java
+++ thread/multithreading/MultithreadThing.java
@@ -9,7 +9,6 @@ public class MultithreadThing extends Thread{
                  catch(InterruptedException e){
                        e.printStackTrace();
                  }
-
              }
       }
 }
